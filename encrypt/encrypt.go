4dc6c71545ca89ff67a295fb18cd5e70c913fd0e
kenjitheman <kenjitheman@proton.me>
2023-09-04 05:56:46 +0300

    feat: aes encryption/decryption implemented

--- encrypt/encrypt.go
+++ encrypt/encrypt.go
@@ -1,1 +1,26 @@
 package encrypt
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"io"
+)
+
+func EncryptAES(key []byte, plaintext []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(ciphertext[aes.BlockSize:], plaintext)
+
+	return ciphertext, nil
+}
