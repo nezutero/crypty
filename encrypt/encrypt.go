32bf8697b11f02df93d4019daf43c0f111e732ab
kenjitheman <kenjitheman@proton.me>
2023-09-04 06:03:00 +0300

    feat: des encryption/decryption

--- encrypt/encrypt.go
+++ encrypt/encrypt.go
@@ -3,7 +3,9 @@ package encrypt
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/des"
 	"crypto/rand"
+	"fmt"
 	"io"
 )
 
@@ -21,6 +23,26 @@ func EncryptAES(key []byte, plaintext []byte) ([]byte, error) {
 
 	mode := cipher.NewCBCEncrypter(block, iv)
 	mode.CryptBlocks(ciphertext[aes.BlockSize:], plaintext)
+
+	return ciphertext, nil
+}
+
+func EncryptDES(key []byte, plaintext []byte) ([]byte, error) {
+	block, err := des.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	blockSize := block.BlockSize()
+	if len(plaintext)%blockSize != 0 {
+		return nil, fmt.Errorf("plaintext is not a multiple of the block size")
+	}
+
+	ciphertext := make([]byte, len(plaintext))
+
+	for i := 0; i < len(plaintext); i += blockSize {
+		block.Encrypt(ciphertext[i:i+blockSize], plaintext[i:i+blockSize])
+	}
 
 	return ciphertext, nil
 }
