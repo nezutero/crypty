8f0003e11a2c7c42c9489ee2ea2fc9396f15b12d
kenjitheman <kenjitheman@proton.me>
2023-09-05 19:48:04 +0300

    feat: sha1 hash func

--- hashing/hashing.go
+++ hashing/hashing.go
@@ -1,7 +1,9 @@
 package hashing
 
 import (
+	"crypto/sha1"
 	"crypto/sha256"
+
 	"golang.org/x/crypto/sha3"
 )
 
@@ -13,6 +15,12 @@ func CalculateSHA256(data []byte) []byte {
 
 func CalculateSHA3_256(data []byte) []byte {
 	hasher := sha3.New256()
+	hasher.Write(data)
+	return hasher.Sum(nil)
+}
+
+func CalculateSHA1(data []byte) []byte {
+	hasher := sha1.New()
 	hasher.Write(data)
 	return hasher.Sum(nil)
 }
