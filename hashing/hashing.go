8280a27e794cc53db6ab85c59dea08c345c1c455
kenjitheman <kenjitheman@proton.me>
2023-09-05 20:07:57 +0300

    chore: sequence of functions changed

--- hashing/hashing.go
+++ hashing/hashing.go
@@ -15,6 +15,14 @@ func CalculateSHA256(data []byte) []byte {
 	return hasher.Sum(nil)
 }
 
+func CalculateSHA224(data []byte) []byte {
+	hasher := sha256.New()
+	hasher.Write(data)
+	sha256Hash := hasher.Sum(nil)
+	sha224Hash := sha256Hash[:28]
+	return sha224Hash
+}
+
 func CalculateSHA3_256(data []byte) []byte {
 	hasher := sha3.New256()
 	hasher.Write(data)
@@ -43,12 +51,4 @@ func CalculateSHA384(data []byte) []byte {
 	hasher := sha512.New384()
 	hasher.Write(data)
 	return hasher.Sum(nil)
-}
-
-func CalculateSHA224(data []byte) []byte {
-	hasher := sha256.New()
-	hasher.Write(data)
-	sha256Hash := hasher.Sum(nil)
-	sha224Hash := sha256Hash[:28]
-	return sha224Hash
 }
