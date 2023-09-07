758ba670941f083df20702724190be4bcad326d0
kenjitheman <kenjitheman@proton.me>
2023-09-07 10:19:19 +0300

    feat: generate rsa key pairs func implemented

--- digital_signatures/digital_signatures.go
+++ digital_signatures/digital_signatures.go
@@ -1,1 +1,15 @@
 package digital_signatures
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+)
+
+func GenerateRSAKeyPair(bits int) (*rsa.PrivateKey, *rsa.PublicKey, error) {
+	privateKey, err := rsa.GenerateKey(rand.Reader, bits)
+	if err != nil {
+		return nil, nil, err
+	}
+	publicKey := &privateKey.PublicKey
+	return privateKey, publicKey, nil
+}
